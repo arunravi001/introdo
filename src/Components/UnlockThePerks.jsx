0490149b4aa09717bba609fa02a768264bf49c17
arunravi001 <arunravi001@gmail.com>
2024-09-29 10:53:36 +0530

    updated

--- src/Components/UnlockThePerks.jsx
+++ src/Components/UnlockThePerks.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import './UnlockThePerks.css';
 import Timer from './Timer'; // Import Timer component
 import ProgressBar from './ProgressBar'; // Import ProgressBar component
+import GpsPayroll from '../assets/images/gps-payroll.png'
+import LegacyHarmony from '../assets/images/lh-image.png'
+import RsCompliance from '../assets/images/rsc-image.png'
+import CloudPowered from '../assets/images/cps-image.png'
 
 const UnlockThePerks = () => {
   const [progress, setProgress] = useState(0);
@@ -26,9 +30,7 @@ const UnlockThePerks = () => {
       <h2>Global Payroll Simplicity</h2>
         <p>Manage multi-currency payroll across the GCC and beyond, in English & Arabic. Seamless onboarding included!</p>
         <div className="payroll">
-          <div className="country"><span className="flag">🇦🇪</span> AED 20,178.04</div>
-          <div className="country"><span className="flag">🇸🇦</span> SAR 73,010.00</div>
-          <div className="country"><span className="flag">🇴🇲</span> OMR 7,603.00</div>
+          <img src={GpsPayroll} alt="GPS Payroll" />
         </div>
       </div>
 
@@ -50,8 +52,8 @@ const UnlockThePerks = () => {
       </div>
 
       <div className=" blue-gradient">
-        <h2>Legacy Harmony</h2>
-        <p>Maximize operational efficiency with zero data loss. Sync all your business tools with an open API for seamless data integration.</p>
+      <h2>Smart Insights</h2>
+      <p>Craft customized reports and dashboards to steer data-driven decisions. All your historic employee data within one end-to-end solution.</p>
         <div className="tools">
           
         </div>
@@ -60,23 +62,36 @@ const UnlockThePerks = () => {
 
       
       <div className="card pink-gradient">
-        <h2>Rock-Solid Compliance</h2>
-        <p>Stay GCC law-aligned amidst changing regulations. Worry-free compliance across locations, preventing penalties and legal hassles.</p>
+      <h2>Legacy Harmony</h2>
+      <p>Maximize operational efficiency with zero data loss. Sync all your business tools with an open API for seamless data integration.</p>
         <div className="badges">
+          <img src={LegacyHarmony} alt="GPS Payroll" />
         </div>
       </div>
 
       <div className="card light-blue-gradient">
-        <h2>Smart Insights</h2>
-        <p>Craft customized reports and dashboards to steer data-driven decisions. All your historic employee data within one end-to-end solution.</p>
+      <div className="rsc-box">
+        <div className="rsc-text">
+          <h2>Rock-Solid Compliance</h2>
+          <p>Stay GCC law-aligned amidst changing regulations. Worry-free compliance across locations, preventing penalties and legal hassles.</p>
+        </div>
+        <div className="rsc-image">
+          <img src={RsCompliance} alt="Rock-Solid Compliance" />
+        </div>
+        </div>
       </div>
 
       
 
       <div className="card purple-blue-gradient">
+      <div className="cps-box">
+        <div className="cps-image">
+          <img src={CloudPowered} alt="Cloud-Powered Savings" />
+        </div>
+        <div className="cps-text">
         <h2>Cloud-Powered Savings</h2>
         <p>Centralize people management with one HR software. Save time and cut costs by leveraging cloud technology.</p>
-        <div className="user-image">
+        </div>
         </div>
       </div>
 
